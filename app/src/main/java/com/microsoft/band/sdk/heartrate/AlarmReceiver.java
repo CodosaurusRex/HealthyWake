355f7dcb7478307e129a2546a7b178acc1716b26
Willie Zhu <zhuwillie11@gmail.com>
2016-02-13 21:16:27 -0500

    Add ability to read from user's alarms

---
+++ app/src/main/java/com/microsoft/band/sdk/heartrate/AlarmReceiver.java
@@ -0,0 +1,14 @@
+package com.microsoft.band.sdk.heartrate;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class AlarmReceiver extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_receiver);
+    }
+
+}
